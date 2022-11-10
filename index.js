7ecfe9b5f4d42f6650a7efb233cccfdf34b94e5a
Bastienne-Banco Théo <theo.bastienne-banco@etu.univ-lyon1.fr>
2022-11-10 11:49:26 +0100

    deploiement

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 
+app.get("/", (req, res) => {
+  res.json({ message: "server!" });
+});
+
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
